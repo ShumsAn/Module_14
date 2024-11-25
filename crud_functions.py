bfd083027cb7c6cdf5fdfdb8a86f82795fa02d0c
ShumsAn <shum-sol@outlook.com>
2024-11-25 13:21:44 +0300

    Задача "Регистрация покупателей":
    Цель: написать простейшие CRUD функции для взаимодействия с базой данных.

--- crud_functions.py
+++ crud_functions.py
@@ -1,8 +1,8 @@
 import sqlite3
 
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor()
 def initiate_db():
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products(
     id INTEGER PRIMARY KEY,
@@ -12,18 +12,43 @@ def initiate_db():
     );
     ''')
     connection.commit()
-    connection.close()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL)
+    ''')
+    connection.commit()
+
 def get_all_products():
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
     cursor.execute("SELECT * FROM Products")
     products = cursor.fetchall()
     return products
+
+def is_included(username):
+    """Проверка пользователя на присутствие в базе"""
+    check_user = cursor.execute('SELECT username FROM Users WHERE username = ?',(f'{username}',))
+    if check_user.fetchone() is None:
+        return False
+    else:
+        return True
+def add_user(username, email, age):
+    """Функция Добавления пользователя"""
+    if is_included(username):
+        print("Пользователь Существует")
+    else:
+        cursor.execute("INSERT INTO Users (username, email,age,balance) VALUES (?,?,?,?)",
+                       (username, email, age,1000))
+        connection.commit()
+
 def add_product(title,description,price):
     """Функция добавления продуктов"""
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
     cursor.execute("INSERT INTO Products (title, description,price) VALUES (?,?,?)",
                    (title, description,price))
+
     connection.commit()
-    connection.close()
+
+
+
